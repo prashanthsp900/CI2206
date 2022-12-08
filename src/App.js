a0a6d631e3c4e0e253baccf63798261e3f293293
prashanth <prashanthsp900@gmail.com>
2022-12-08 16:17:59 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>This is Continuous Integration</h1>
+        <h1>Hello World! This is local Repository</h1>
     </div>
   );
 }
